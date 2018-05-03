fdfabd40057ec2038440d6d72a8be47591d579d7
Your Name <zxj@example.com>
2018-05-03 15:26:16 +0800

    添加依赖库，加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/zxj/coolweather/db/City.java
@@ -0,0 +1,47 @@
+package com.example.zxj.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zxj on 2018/5/3.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/zxj/coolweather/db/Country.java
@@ -0,0 +1,47 @@
+package com.example.zxj.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zxj on 2018/5/3.
+ */
+
+public class Country extends DataSupport {
+
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
