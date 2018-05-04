5b78daa2babe8f8035ff401abceb96222bf7e183
Your Name <zxj@example.com>
2018-05-04 14:18:05 +0800

    增加后台自动更新功能

--- app/src/main/java/com/example/zxj/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/zxj/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.zxj.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.Image;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.zxj.coolweather.gson.Forecast;
 import com.example.zxj.coolweather.gson.Weather;
+import com.example.zxj.coolweather.service.AutoUpdateService;
 import com.example.zxj.coolweather.util.HttpUtil;
 import com.example.zxj.coolweather.util.Utility;
 
@@ -232,5 +234,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
